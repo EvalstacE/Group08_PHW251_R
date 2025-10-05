00d2be846eaba8c9edf1d2e7eb53c3a840c1ad41
EvalstacE <valstacey01@gmail.com>
2025-10-04 19:27:34 -0600

    + added global.R file

---
+++ global.R
@@ -0,0 +1,11 @@
+
+install.packages("pacman")
+
+
+# Load and install packages
+pacman::p_load(
+  dplyr, tidyr, ggplot2, lubridate, ggthemes, cowplot, readr, rlang,
+  classInt, tidycensus, sf, here, stringr, purrr, svglite, rmapshaper,
+  scales, ggrepel, viridis, RColorBrewer, maps, ggfx, glue
+)
+
