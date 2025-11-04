24932883c24154a64d89f5cef7034f08515c04af
Valerie Stacey <VSTACEY@lccountymt.gov>
2025-11-04 16:35:50 -0700

    + more drafting of milestone 3
    + _data folder now has two sub-directories
    --- "raw_data" - folder with original datasets (exception being cleaned column names)
    
    --- "cleaned_data" - folder with cleaned databases for each of the 3, and a single combined dataframe that binded LA County data with CA database ("combinded_df.csv")
    
    -- two .qmd files to separate out 1) cleaning and restructuring the raw databases ("03a_cleaning.qmd), and 2) descriptive statistics and explorations ("03b_descriptive_stats.qmd)
    
    -- the final .qmd file will knit these two together into a single final report

--- global.R
+++
@@ -1,9 +0,0 @@
-
-
-# Load and install packages
-pacman::p_load(
-  dplyr, tidyr, ggplot2, lubridate, ggthemes, cowplot, readr, rlang, purrr, rlang,
-  classInt, tidycensus, sf, here, stringr, purrr, svglite, rmapshaper, readxl,
-  scales, ggrepel, viridis, RColorBrewer, maps, ggfx, glue, skimr, DataExplorer,
-  knitr, kableExtra
-)
